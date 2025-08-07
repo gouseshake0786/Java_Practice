dc5fc8c1c7a8752bd20d175fd95bf30f663a72da
Mahamad Gouse <mahammadgouse432@gmail.com>
2025-08-07 18:24:01 +0530

    Dice problem

--- src/com/mahamad/Dice.java
+++ src/com/mahamad/Dice.java
@@ -6,13 +6,15 @@ import java.util.List;
 //Amazon
 public class Dice {
     public static void main(String[] args) {
-        //dice("", 4);
+        dice("", 4);
         System.out.println(diceRet("", 4));
         System.out.println();
         System.out.println(diceFaceRet("", 3, 9));
+        System.out.println();
+        diceFace("", 4, 7);
     }
 
-    private static void dice(String p, int target) {
+    static void dice(String p, int target) {
         if (target == 0) {
             System.out.println(p);
             return;
@@ -24,7 +26,7 @@ public class Dice {
     }
 
     //if dice face is more than 6
-    private static void diceFace(String p, int target, int face) {
+    static void diceFace(String p, int target, int face) {
         if (target == 0) {
             System.out.println(p);
             return;
@@ -35,7 +37,7 @@ public class Dice {
         }
     }
 
-    private static List<String> diceRet(String p, int target) {
+    static List<String> diceRet(String p, int target) {
         if (target == 0) {
             List<String> list = new ArrayList<>();
             list.add(p);
@@ -48,7 +50,7 @@ public class Dice {
         return ans;
     }
 
-    private static List<String> diceFaceRet(String p, int target, int face) {
+    static List<String> diceFaceRet(String p, int target, int face) {
         if (target == 0) {
             List<String> list = new ArrayList<>();
             list.add(p);

d914adbb512a219e87680415d01c0f36acf7edff
Mahamad Gouse <mahammadgouse432@gmail.com>
2025-08-07 18:07:48 +0530

    Dice problem

---
+++ src/com/mahamad/Dice.java
@@ -0,0 +1,63 @@
+package com.mahamad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Amazon
+public class Dice {
+    public static void main(String[] args) {
+        //dice("", 4);
+        System.out.println(diceRet("", 4));
+        System.out.println();
+        System.out.println(diceFaceRet("", 3, 9));
+    }
+
+    private static void dice(String p, int target) {
+        if (target == 0) {
+            System.out.println(p);
+            return;
+        }
+
+        for (int i = 1; i <= 6 && i <= target; i++) {
+            dice(p + i, target - i);
+        }
+    }
+
+    //if dice face is more than 6
+    private static void diceFace(String p, int target, int face) {
+        if (target == 0) {
+            System.out.println(p);
+            return;
+        }
+
+        for (int i = 1; i <= face && i <= target; i++) {
+            diceFace(p + i, target - i, face);
+        }
+    }
+
+    private static List<String> diceRet(String p, int target) {
+        if (target == 0) {
+            List<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        List<String> ans = new ArrayList<>();
+        for (int i = 1; i <= 6 && i <= target; i++) {
+            ans.addAll(diceRet(p + i, target - i));
+        }
+        return ans;
+    }
+
+    private static List<String> diceFaceRet(String p, int target, int face) {
+        if (target == 0) {
+            List<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        List<String> ans = new ArrayList<>();
+        for (int i = 1; i <= face && i <= target; i++) {
+            ans.addAll(diceFaceRet(p + i, target - i, face));
+        }
+        return ans;
+    }
+}
